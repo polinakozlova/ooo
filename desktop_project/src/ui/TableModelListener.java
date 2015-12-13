b5640a39735c38902089e9147f78990e944c03f8
Yannick Crabbé <yannickcrabbe@gmail.com>
2015-12-13 10:44:36 +0100

    Delete TableModelListener.java

--- desktop_project/src/ui/TableModelListener.java
+++
@@ -1,8 +0,0 @@
-package ui;
-
-import java.util.EventListener;
-import javax.swing.event.TableModelEvent;
-
-public interface TableModelListener extends EventListener {
-	  public void tableChanged(TableModelEvent e);
-	}
